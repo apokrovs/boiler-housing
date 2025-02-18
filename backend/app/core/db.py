13e90a57e07513388344699e7b22557a5d66b394
Arnav Wadhwa <arnav.v.wadhwa@gmail.com>
2025-02-18 09:39:38 -0500

    Template is now functioning correctly

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session, create_engine, select
 
-from app import crud
+from app.crud import crud
 from app.core.config import settings
 from app.models.models import User, UserCreate
 
@@ -9,7 +9,7 @@ engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 # make sure all SQLModel models are imported (app.models) before initializing DB
 # otherwise, SQLModel might fail to initialize relationships properly
-# for more details: https://github.com/tiangolo/full-stack-fastapi-template/issues/28
+# for more details: https://github.com/fastapi/full-stack-fastapi-template/issues/28
 
 
 def init_db(session: Session) -> None:
@@ -18,16 +18,15 @@ def init_db(session: Session) -> None:
     # the tables un-commenting the next lines
     # from sqlmodel import SQLModel
 
-    # from app.core.engine import engine
     # This works because the models are already imported and registered from app.models
     # SQLModel.metadata.create_all(engine)
 
     user = session.exec(
-        select(User).where(User.username == settings.FIRST_SUPERUSER)
+        select(User).where(User.email == settings.FIRST_SUPERUSER)
     ).first()
     if not user:
         user_in = UserCreate(
-            username=settings.FIRST_SUPERUSER,
+            email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
--- backend/app/tests/api/routes/test_users.py
+++ backend/app/tests/api/routes/test_users.py
@@ -4,10 +4,10 @@ from unittest.mock import patch
 from fastapi.testclient import TestClient
 from sqlmodel import Session, select
 
-from app import crud
+from app.crud import crud
 from app.core.config import settings
 from app.core.security import verify_password
-from app.models import User, UserCreate
+from app.models.models import User, UserCreate
 from app.tests.utils.utils import random_email, random_lower_string
 
 
