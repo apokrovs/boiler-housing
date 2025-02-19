b59a83a81851cb0b8fcc93587b4255f7126678b7
wyatt-brooks <142945761+wyatt-brooks@users.noreply.github.com>
2025-02-19 12:19:27 -0500

    Login front-end done. Functionality is commented out for now

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,144 +1,102 @@
-import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons"
+//import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons"
 import {
+  Box,
   Button,
-  Container,
-  FormControl,
-  FormErrorMessage,
-  Icon,
+  //Container,
+  //FormControl,
+  //FormErrorMessage,
+  Heading,
+  HStack,
+  //Icon,
   Image,
   Input,
-  InputGroup,
-  InputRightElement,
+  //InputGroup,
+  //InputRightElement,
   Link,
   Text,
-  useBoolean,
+    Stack,
+    Center,
+  //useBoolean,
 } from "@chakra-ui/react"
 import {
   Link as RouterLink,
   createFileRoute,
-  redirect,
+  //redirect,
 } from "@tanstack/react-router"
-import { type SubmitHandler, useForm } from "react-hook-form"
+//import { type SubmitHandler, useForm } from "react-hook-form"
 
-import Logo from "/assets/images/fastapi-logo.svg"
-import type { Body_login_login_access_token as AccessToken } from "../client"
-import useAuth, { isLoggedIn } from "../hooks/useAuth"
-import { emailPattern } from "../utils"
+ import Logo from "/assets/images/BoilerHousingCropped.png"
+//import type { Body_login_login_access_token as AccessToken } from "../client"
+//import useAuth, { isLoggedIn } from "../hooks/useAuth"
+//import { emailPattern } from "../utils"
 
 export const Route = createFileRoute("/login")({
   component: Login,
   beforeLoad: async () => {
-    if (isLoggedIn()) {
-      throw redirect({
-        to: "/",
-      })
-    }
+    // if (isLoggedIn()) {
+    //   throw redirect({
+    //     to: "/",
+    //   })
+    // }
   },
 })
 
 function Login() {
-  const [show, setShow] = useBoolean()
-  const { loginMutation, error, resetError } = useAuth()
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<AccessToken>({
-    mode: "onBlur",
-    criteriaMode: "all",
-    defaultValues: {
-      username: "",
-      password: "",
-    },
-  })
-
-  const onSubmit: SubmitHandler<AccessToken> = async (data) => {
-    if (isSubmitting) return
-
-    resetError()
-
-    try {
-      await loginMutation.mutateAsync(data)
-    } catch {
-      // error is handled by useAuth hook
-    }
-  }
+  // const [show, setShow] = useBoolean()
+  // const { loginMutation, error, resetError } = useAuth()
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors, isSubmitting },
+  // } = useForm<AccessToken>({
+  //   mode: "onBlur",
+  //   criteriaMode: "all",
+  //   defaultValues: {
+  //     username: "",
+  //     password: "",
+  //   },
+  // })
+  //
+  // const onSubmit: SubmitHandler<AccessToken> = async (data) => {
+  //   if (isSubmitting) return
+  //
+  //   resetError()
+  //
+  //   try {
+  //     await loginMutation.mutateAsync(data)
+  //   } catch {
+  //     // error is handled by useAuth hook
+  //   }
+  // }
 
   return (
-    <>
-      <Container
-        as="form"
-        onSubmit={handleSubmit(onSubmit)}
-        h="100vh"
-        maxW="sm"
-        alignItems="stretch"
-        justifyContent="center"
-        gap={4}
-        centerContent
-      >
-        <Image
-          src={Logo}
-          alt="FastAPI logo"
-          height="auto"
-          maxW="2xs"
-          alignSelf="center"
-          mb={4}
-        />
-        <FormControl id="username" isInvalid={!!errors.username || !!error}>
-          <Input
-            id="username"
-            {...register("username", {
-              required: "Username is required",
-              pattern: emailPattern,
-            })}
-            placeholder="Email"
-            type="email"
-            required
-          />
-          {errors.username && (
-            <FormErrorMessage>{errors.username.message}</FormErrorMessage>
-          )}
-        </FormControl>
-        <FormControl id="password" isInvalid={!!error}>
-          <InputGroup>
-            <Input
-              {...register("password", {
-                required: "Password is required",
-              })}
-              type={show ? "text" : "password"}
-              placeholder="Password"
-              required
-            />
-            <InputRightElement
-              color="ui.dim"
-              _hover={{
-                cursor: "pointer",
-              }}
-            >
-              <Icon
-                as={show ? ViewOffIcon : ViewIcon}
-                onClick={setShow.toggle}
-                aria-label={show ? "Hide password" : "Show password"}
-              >
-                {show ? <ViewOffIcon /> : <ViewIcon />}
-              </Icon>
-            </InputRightElement>
-          </InputGroup>
-          {error && <FormErrorMessage>{error}</FormErrorMessage>}
-        </FormControl>
-        <Link as={RouterLink} to="/recover-password" color="blue.500">
-          Forgot password?
-        </Link>
-        <Button variant="primary" type="submit" isLoading={isSubmitting}>
-          Log In
-        </Button>
-        <Text>
-          Don't have an account?{" "}
-          <Link as={RouterLink} to="/signup" color="blue.500">
-            Sign up
-          </Link>
-        </Text>
-      </Container>
-    </>
-  )
+      <>
+        <Box
+            boxShadow={'md'}
+            height={"100px"}
+            background={"black"}
+            width={"100%"}>
+          <HStack gap={90}>
+            <Image pl={4} pt={"15px"} src={Logo}></Image>
+          </HStack>
+
+        </Box>
+        <Center p={12} bg={'white'}>
+          <Stack
+              rounded={'lg'} boxShadow={'lg'} bg={"white"} p={20} gap={6}>
+            <Heading fontSize={"3xl"} color={"#373A36"}>Welcome Back!</Heading>
+            <Text color={"#373A36"} fontSize={"md"}>Please log in with your email and password.</Text>
+            <Input placeholder={"Email"}></Input>
+            <Input type={"password"} placeholder={"Password"}></Input>
+            <HStack gap={19}>
+              <Link as={RouterLink} to={"/recover-password"} color={'#C5A939'} fontSize={'md'}>Forgot Password?</Link>
+              <Button bg={"black"} loadingText={"Signing you in..."} width={"50%"} size={'lg'} color={'#C5A939'}>Sign in</Button>
+            </HStack>
+            <HStack>
+              <Text color={"#373A36"} fontSize={"md"}>New here?</Text>
+              <Link as={RouterLink} to={"/signup"} variant={'underline'} color={'#C5A939'} fontSize={'md'}>Create an account</Link></HStack>
+          </Stack>
+        </Center>
+      </>
+  );
 }
