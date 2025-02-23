c3e2fb8125ab5db9d5e549c2b7c994c3a66f0378
Arnav Wadhwa <arnav.v.wadhwa@gmail.com>
2025-02-23 14:58:38 -0500

    Fixed backend circular imports.

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session, create_engine, select
 
-from app.crud import crud
+from app.crud import users as crud_users
 from app.core.config import settings
 from app.models.users import User, UserCreate
 
@@ -30,4 +30,4 @@ def init_db(session: Session) -> None:
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
-        user = crud.create_user(session=session, user_create=user_in)
+        user = crud_users.create_user(session=session, user_create=user_in)
