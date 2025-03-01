dc4a1c57e86712c15d15d7600ec12042781b1ab7
luptona <luptona@purdue.edu>
2025-03-01 14:27:31 -0500

    Updated Login UI to include choosing login via password or pin

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,168 +1,203 @@
 //import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons"
 import {
-  Box,
-  Button,
-  //Container,
-  FormControl,
-  //FormErrorMessage,
-  Heading,
-  HStack,
-  //Icon,
-  Image,
-  Input,
-  Link,
-  Text,
-  Stack,
-  Center,
-  Alert,
-  AlertIcon, FormErrorMessage
-  //useBoolean
+    Box,
+    Button,
+    //Container,
+    FormControl,
+    //FormErrorMessage,
+    Heading,
+    HStack,
+    //Icon,
+    Image,
+    Input,
+    Link,
+    Text,
+    Stack,
+    Center,
+    Alert,
+    AlertIcon, FormErrorMessage,
+    Radio, RadioGroup
+    //useBoolean
 } from "@chakra-ui/react"
 import {
-  Link as RouterLink,
-  createFileRoute,
-  redirect,
+    Link as RouterLink,
+    createFileRoute,
+    redirect,
 } from "@tanstack/react-router"
-import { type SubmitHandler, useForm } from "react-hook-form"
+import {type SubmitHandler, useForm} from "react-hook-form"
 
 //const Logo = "/assets/images/BoilerHousingCropped.png"
-import type { Body_login_login_access_token as AccessToken } from "../client"
-import useAuth, { isLoggedIn } from "../hooks/useAuth"
-import { emailPattern } from "../utils"
+import type {Body_login_login_access_token as AccessToken} from "../client"
+import useAuth, {isLoggedIn} from "../hooks/useAuth"
+import {emailPattern} from "../utils"
+import {useState} from "react";
 
 export const Route = createFileRoute("/login")({
-  component: Login,
-  beforeLoad: async () => {
-    if (isLoggedIn()) {
-      throw redirect({
-        to: "/",
-      })
-    }
-  },
+    component: Login,
+    beforeLoad: async () => {
+        if (isLoggedIn()) {
+            throw redirect({
+                to: "/",
+            })
+        }
+    },
 })
 
 function Login() {
-  //const [show, setShow] = useBoolean()
-  const { loginMutation, error, resetError } = useAuth()
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<AccessToken>({
-    mode: "onBlur",
-    criteriaMode: "all",
-    defaultValues: {
-      username: "",
-      password: "",
-    },
-  })
+    //const [show, setShow] = useBoolean()
+    const {loginMutation, error, resetError} = useAuth()
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting},
+    } = useForm<AccessToken>({
+        mode: "onBlur",
+        criteriaMode: "all",
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    })
 
-  const onSubmit: SubmitHandler<AccessToken> = async (data) => {
-    if (isSubmitting) return
+    const [loginMethod, setLoginMethod] = useState<"password" | "pin">("password")
 
-    resetError()
 
-    try {
-      await loginMutation.mutateAsync(data)
-      //throw redirect({to: "/settings"})
-    } catch {
-      // error is handled by useAuth hook
+    const onSubmit: SubmitHandler<AccessToken> = async (data) => {
+        if (isSubmitting) return
+
+        resetError()
+
+        try {
+            await loginMutation.mutateAsync(data)
+            //throw redirect({to: "/settings"})
+        } catch {
+            // error is handled by useAuth hook
+        }
     }
-  }
 
-  return (
-      <>
-        <Box
-            boxShadow={'md'}
-            height={"100px"}
-            //background={"black"}
-            width={"100%"}>
-          <HStack gap={90}>
-            <Image pl={4} pt={"15px"} src={"/assets/images/BoilerHousingCropped.png"}></Image>
-          </HStack>
-        </Box>
-        <Center p={12} //bg={'white'}
-        >
-          <Stack
-              as = {"form"}
-              onSubmit = {handleSubmit(onSubmit)}
-              rounded={'lg'}
-              boxShadow={'lg'}
-              //bg={"white"}
-              p={20}
-              gap={6}>
-            <Heading
-                fontSize={"3xl"}
-                color={"#CEB888"}
-                >
-              Welcome Back!
-            </Heading>
-            <Text
-                //color={"#373A36"}
-                fontSize={"md"}>
-              Please log in with your email and password.
-            </Text>
-            {error && (
-              <Alert status="error">
-                <AlertIcon />
-                {error}
-            </Alert>
-          )}
-            <FormControl id = {"username"} isInvalid={!!errors.username || !!error}>
-            <Input
-                {...register("username", {
-                  required: "Username is required",
-                  pattern: emailPattern,
-                })}
-                placeholder={"Email"}
-                type={"email"}
-                id={"username"}
-                >
-            </Input>
-              {errors.username && (
-                <FormErrorMessage>{errors.username.message}</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl id={"password"} isInvalid = {!!error}>
-            <Input
-                {...register("password", {required: "Password is required"})}
-                placeholder={"Password"}
-                type={"password"}>
-              </Input>
-            </FormControl>
-            <HStack gap={19}>
-              <Link
-                  as={RouterLink}
-                  to={"/recover-password"}
-                  color={'#CEB888'} fontSize={'md'}>
-                Forgot Password?
-              </Link>
-              <Button
-                  type={"submit"}
-                  //bg={"black"}
-                  loadingText={"Logging you in..."}
-                  isLoading={isSubmitting}
-                  width={"50%"}
-                  size={'lg'}
-                  color={'#CEB888'}>Log In</Button>
-            </HStack>
-            <HStack>
-              <Text
-                  //color={"#373A36"}
-                  fontSize={"md"}>
-                New here?
-              </Text>
-              <Link
-                  as={RouterLink}
-                  to={"/signup"}
-                  variant={'underline'}
-                  color={'#CEB888'}
-                  fontSize={'md'}>
-                Create an account
-              </Link>
-            </HStack>
-          </Stack>
-        </Center>
-      </>
-  );
+    return (
+        <>
+            <Box
+                boxShadow={'md'}
+                height={"100px"}
+                //background={"black"}
+                width={"100%"}>
+                <HStack gap={90}>
+                    <Image pl={4} pt={"15px"} src={"/assets/images/BoilerHousingCropped.png"}></Image>
+                </HStack>
+            </Box>
+            <Center p={12} //bg={'white'}
+            >
+                <Stack
+                    as={"form"}
+                    onSubmit={handleSubmit(onSubmit)}
+                    rounded={'lg'}
+                    boxShadow={'lg'}
+                    //bg={"white"}
+                    p={20}
+                    gap={6}>
+                    <Heading
+                        fontSize={"3xl"}
+                        color={"#CEB888"}
+                    >
+                        Welcome Back!
+                    </Heading>
+                    <Text
+                        //color={"#373A36"}
+                        fontSize={"md"}>
+                        Please log in with your email and {loginMethod === "password" ? "password" : "PIN"}.
+                    </Text>
+                    {error && (
+                        <Alert status="error">
+                            <AlertIcon/>
+                            {error}
+                        </Alert>
+                    )}
+                    <FormControl id={"username"} isInvalid={!!errors.username || !!error}>
+                        <Input
+                            {...register("username", {
+                                required: "Username is required",
+                                pattern: emailPattern,
+                            })}
+                            placeholder={"Email"}
+                            type={"email"}
+                            id={"username"}
+                        >
+                        </Input>
+                        {errors.username && (
+                            <FormErrorMessage>{errors.username.message}</FormErrorMessage>
+                        )}
+                    </FormControl>
+                    {loginMethod === "password" && (
+                        <FormControl id={"password"} isInvalid={!!error}>
+                            <Input
+                                {...register("password", {required: "Password is required"})}
+                                placeholder={"Password"}
+                                type={"password"}
+                            />
+                        </FormControl>
+                    )}
+
+                    {loginMethod === "pin" && (
+                        <FormControl id={"pin"} isInvalid={!!error}>
+                            <Input
+                                {...register("password", {required: "PIN is required"})}
+                                placeholder={"PIN"}
+                                type={"password"}
+                                maxLength={4} // Limit to 4 digits for PIN
+                            />
+                        </FormControl>
+                    )}
+                    <HStack gap={19}>
+                        <Link
+                            as={RouterLink}
+                            to={"/recover-password"}
+                            color={'#CEB888'} fontSize={'md'}>
+                            Forgot Password?
+                        </Link>
+                        <Button
+                            type={"submit"}
+                            //bg={"black"}
+                            loadingText={"Logging you in..."}
+                            isLoading={isSubmitting}
+                            width={"50%"}
+                            size={'lg'}
+                            color={'#CEB888'}>Log In</Button>
+                    </HStack>
+                    <HStack>
+                        <Text
+                            //color={"#373A36"}
+                            fontSize={"md"}>
+                            New here?
+                        </Text>
+                        <Link
+                            as={RouterLink}
+                            to={"/signup"}
+                            variant={'underline'}
+                            color={'#CEB888'}
+                            fontSize={'md'}>
+                            Create an account
+                        </Link>
+                    </HStack>
+
+                    <FormControl>
+                        <RadioGroup
+                            value={loginMethod}
+                            onChange={(value) => setLoginMethod(value as "password" | "pin")}
+                            display="flex"
+                            flexDirection="row"
+                            justifyContent="space-evenly"
+                        >
+                            <Radio value="password" colorScheme="teal">
+                                Password
+                            </Radio>
+                            <Radio value="pin" colorScheme="teal">
+                                PIN
+                            </Radio>
+                        </RadioGroup>
+                    </FormControl>
+                </Stack>
+            </Center>
+        </>
+    );
 }
