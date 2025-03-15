b9390bff96e1b3093853ab310ba53e4c6f36368f
luptona <luptona@purdue.edu>
2025-03-15 15:04:14 -0500

    Completed listing backend with creating/updating/removing functionality & updated database to include listing table

---
+++ backend/app/api/routes/listings.py
@@ -0,0 +1,110 @@
+import uuid
+from typing import Any
+
+from fastapi import APIRouter, HTTPException
+from sqlmodel import func, select
+
+from app.api.deps import CurrentUser, SessionDep
+from app.models.listings import Listing, ListingCreate, ListingPublic, ListingsPublic, ListingUpdate
+from app.models.utils import Message
+
+router = APIRouter(prefix="/listings", tags=["listings"])
+
+
+@router.get("/", response_model=ListingsPublic)
+def read_listings(
+    session: SessionDep, current_user: CurrentUser, skip: int = 0, limit: int = 100
+) -> Any:
+    """
+    Retrieve listings.
+    """
+
+    if current_user.is_superuser:
+        count_statement = select(func.count()).select_from(Listing)
+        count = session.exec(count_statement).one()
+        statement = select(Listing).offset(skip).limit(limit)
+        listings = session.exec(statement).all()
+    else:
+        count_statement = (
+            select(func.count())
+            .select_from(Listing)
+            .where(Listing.owner_id == current_user.id)
+        )
+        count = session.exec(count_statement).one()
+        statement = (
+            select(Listing)
+            .where(Listing.owner_id == current_user.id)
+            .offset(skip)
+            .limit(limit)
+        )
+        listings = session.exec(statement).all()
+
+    return ListingsPublic(data=listings, count=count)
+
+
+@router.get("/{id}", response_model=ListingPublic)
+def read_listing(session: SessionDep, current_user: CurrentUser, id: uuid.UUID) -> Any:
+    """
+    Get listing by ID.
+    """
+    listing = session.get(Listing, id)
+    if not listing:
+        raise HTTPException(status_code=404, detail="Listing not found")
+    if not current_user.is_superuser and (listing.owner_id != current_user.id):
+        raise HTTPException(status_code=400, detail="Not enough permissions")
+    return listing
+
+
+@router.post("/", response_model=ListingPublic)
+def create_listing(
+    *, session: SessionDep, current_user: CurrentUser, listing_in: ListingCreate
+) -> Any:
+    """
+    Create new listing.
+    """
+    listing = Listing.model_validate(listing_in, update={"owner_id": current_user.id})
+    session.add(listing)
+    session.commit()
+    session.refresh(listing)
+    return listing
+
+
+@router.put("/{id}", response_model=ListingPublic)
+def update_listing(
+    *,
+    session: SessionDep,
+    current_user: CurrentUser,
+    id: uuid.UUID,
+    listing_in: ListingUpdate,
+) -> Any:
+    """
+    Update a listing.
+    """
+    listing = session.get(Listing, id)
+    if not listing:
+        raise HTTPException(status_code=404, detail="Listing not found")
+    if not current_user.is_superuser and (listing.owner_id != current_user.id):
+        raise HTTPException(status_code=400, detail="Not enough permissions")
+    update_dict = listing_in.model_dump(exclude_unset=True)
+    listing.sqlmodel_update(update_dict)
+    session.add(listing)
+    session.commit()
+    session.refresh(listing)
+    return listing
+
+
+@router.delete("/{id}")
+def delete_listing(
+    session: SessionDep, current_user: CurrentUser, id: uuid.UUID
+) -> Message:
+    """
+    Delete a listing.
+    """
+    listing = session.get(Listing, id)
+    if not listing:
+        raise HTTPException(status_code=404, detail="Listing not found")
+    if not current_user.is_superuser and (listing.owner_id != current_user.id):
+        raise HTTPException(status_code=400, detail="Not enough permissions")
+    session.delete(listing)
+    session.commit()
+    return Message(message="Listing deleted successfully")
---
+++ backend/app/models/listings.py
@@ -0,0 +1,49 @@
+import uuid
+from typing import Optional, List
+from sqlmodel import Field, Relationship, SQLModel
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import JSONB
+
+# Shared properties
+class ListingBase(SQLModel):
+    num_bedrooms: Optional[List[str]] | None = Field(default=None, sa_column=Column(JSONB))
+    num_bathrooms: Optional[List[str]] | None = Field(default=None, sa_column=Column(JSONB))
+    address: Optional[str] | None = Field(default=None, max_length=255)
+    realty_company: Optional[str] | None = Field(default=None, max_length=255)
+    min_price: Optional[float] | None = Field(default=None, ge=0)
+    max_price: Optional[float] | None = Field(default=None, ge=0)
+    included_utilities: Optional[List[str]] | None = Field(default=None, sa_column=Column(JSONB))
+    security_deposit: Optional[str] | None = Field(default=None)
+    amenities: Optional[List[str]] | None = Field(default=None, sa_column=Column(JSONB))
+    lease_start_date: Optional[str] | None = Field(default=None)
+    lease_end_date: Optional[str] | None = Field(default=None)
+
+
+# Properties to receive on listing creation
+class ListingCreate(ListingBase):
+    pass
+
+
+# Properties to receive on listing update
+class ListingUpdate(ListingBase):
+    pass
+
+
+# Database model, database table inferred from class name
+class Listing(ListingBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    owner_id: uuid.UUID = Field(
+        foreign_key="user.id", nullable=False, ondelete="CASCADE"
+    )
+    owner: Optional["User"] = Relationship(back_populates="listings")
+
+
+# Properties to return via API, id is always required
+class ListingPublic(ListingBase):
+    id: uuid.UUID
+    owner_id: uuid.UUID
+
+
+class ListingsPublic(SQLModel):
+    data: list[ListingPublic]
+    count: int
