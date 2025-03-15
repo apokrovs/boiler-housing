572b36815004ed9aa8327e49f91ca2986cd7ec47
wyatt-brooks <142945761+wyatt-brooks@users.noreply.github.com>
2025-03-15 15:57:01 -0400

    created roommates route and added it to the side bar

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -19,6 +19,7 @@ import { Route as LoginImport } from './routes/login'
 import { Route as LayoutImport } from './routes/_layout'
 import { Route as LayoutIndexImport } from './routes/_layout/index'
 import { Route as LayoutSettingsImport } from './routes/_layout/settings'
+import { Route as LayoutRoommatesImport } from './routes/_layout/roommates'
 import { Route as LayoutMessagingImport } from './routes/_layout/messaging'
 import { Route as LayoutItemsImport } from './routes/_layout/items'
 import { Route as LayoutChatImport } from './routes/_layout/chat'
@@ -63,6 +64,11 @@ const LayoutIndexRoute = LayoutIndexImport.update({
 
 const LayoutSettingsRoute = LayoutSettingsImport.update({
   path: '/settings',
+  getParentRoute: () => LayoutRoute,
+} as any)
+
+const LayoutRoommatesRoute = LayoutRoommatesImport.update({
+  path: '/roommates',
   getParentRoute: () => LayoutRoute,
 } as any)
 
@@ -130,6 +136,10 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof LayoutMessagingImport
       parentRoute: typeof LayoutImport
     }
+    '/_layout/roommates': {
+      preLoaderRoute: typeof LayoutRoommatesImport
+      parentRoute: typeof LayoutImport
+    }
     '/_layout/settings': {
       preLoaderRoute: typeof LayoutSettingsImport
       parentRoute: typeof LayoutImport
@@ -149,6 +159,7 @@ export const routeTree = rootRoute.addChildren([
     LayoutChatRoute,
     LayoutItemsRoute,
     LayoutMessagingRoute,
+    LayoutRoommatesRoute,
     LayoutSettingsRoute,
     LayoutIndexRoute,
   ]),
---
+++ frontend/src/routes/_layout/roommates.tsx
@@ -0,0 +1,22 @@
+import {createFileRoute} from "@tanstack/react-router";
+import {Center, Container, Heading, VStack, Text, Button} from "@chakra-ui/react";
+
+export const Route = createFileRoute("/_layout/roommates")({
+    component: RoommatePage
+});
+
+function RoommatePage() {
+    return (
+        <>
+            <Container maxW="full">
+                <Center>
+                    <VStack>
+                <Heading>Welcome to Roommates!</Heading>
+                    <Text pb = {4} >Please complete the roommate matching quiz to use our roommate services.</Text>
+                        <Button>Take the quiz</Button>
+                    </VStack>
+                </Center>
+            </Container>
+        </>
+    )
+}
